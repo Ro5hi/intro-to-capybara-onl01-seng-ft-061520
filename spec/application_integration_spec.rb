334779a210346f9e4c3158337f9f9e8d0455f79e
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-14 16:56:01 +0000

    Automatically backed up by Learn

--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -6,16 +6,15 @@ describe "GET '/' - Greeting Form" do
     visit '/'
     expect(page.body).to include("Welcome!")
   end
-
+ 
   # New test
   it 'has a greeting form with a user_name field' do
     visit '/'
-
+ 
     expect(page).to have_selector("form")
     expect(page).to have_field(:user_name)
   end
 end
-
 describe "POST '/greet' - User Greeting" do
   it 'greets the user personally based on their user_name in the form' do
     visit '/'
